a725b94805216f47274dfb9b250624aacef6ab3d
Roy Chiu <rychiu13@gmail.com>
2017-07-21 20:30:31 -0500

    Content Addition
    Created projects page, bug fixing on buttons - All buttons work except for contact form, more buttons in project section, and footer list links buttons. Attempted to incorporate contact form validation.

--- js/index.js
+++ js/index.js
@@ -7,6 +7,21 @@ function navigation(){
 	});
 }
 
+// HOME PAGE JS
+function homeNav(){
+	$(document).ready(function(){
+		$(".primary-nav a, .footer-nav a, .main .btn, .about .btn").on('click', function(e) {
+			e.preventDefault();
+	    	var hash = this.hash;
+	    	$('body').animate({
+	    		scrollTop: $(hash).offset().top
+	      	}, 500, function(){
+	    		window.location.hash = hash;
+	      	}); 
+		});
+	});
+}
+
 function projects(){
 	$('.luvtalk-project').css('height', $('.luvtalk-project').width());
 	$('.luvtalk-project h3').css('padding-top', ($('.luvtalk-project').height() - $('.luvtalk-project h3').height() - $('.luvtalk-project .row').outerHeight())/2);
@@ -50,6 +65,55 @@ function dynamicHeights(){ //Resize handler for window onload function
 	$('.contact-form-body').css('padding-top', ($('.contact-instructions').outerHeight()-$('.contact-form-body').height())/2);
 }
 
+function contactValidate(email, firstnameval, lastnameval, msgval){
+	var emailRE = /\S+@\S+\.\S+/;
+	var nameRE = /^[A-Za-z\s]+$/;
+	var emailValidation = emailRE.test(email);
+	var msgValidation = $.trim(msgval == '');
+	var name;
+	if (!nameRE.test(firstnameval) || !nameRE.test(lastnameval)){
+		name = false;
+	}
+	else {
+		name = true;
+	}
+
+	if (emailValidation && name && msgValidation){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function contactFormSubmit(){
+	var firstName = $('.firstname input').value;
+	var lastName = $('.lastname input').value;
+	var prefEmail = $('.email input').value;
+	var msg = $('.message textarea').value;
+	if (contactValidate(prefEmail, firstName, lastName, msg)){
+		var emailBody = 'The following information has been automatically generated through the contact form that was completed. Please do not alter or change this section as I would like to know who I am speaking with! :) \n' +
+			'Full Name: ' + firstName + ' ' + lastName + '\n' +
+			'Reply Email: ' + prefEmail + '\n' +
+			msg;
+		window.location.href = 'mailto: rychiu13@gmail.com?subject=Website Message&body=' + emailBody;
+	}
+	else {
+		if (!/^[A-Za-z\s]+$/.test(firstName) || $.trim(lastName == '')){
+			$('.firstname-error').css('display', 'initial');
+		}
+		if (!/^[A-Za-z\s]+$/.test(lastName) || $.trim(firstName == '')){
+			$('.lastname-error').css('display', 'initial');
+		}
+		if (!/\S+@\S+\.\S+/.test(prefEmail)){
+			$('.email-error').css('display', 'initial');
+		}
+		if ($.trim(msg == '')){
+			$('.message-error').css('display', 'initial');
+		}
+	}
+}
+
 window.onload = function(){
 	//About section
 	//Determine if the image is larger than the description box - larger screen widths
@@ -81,15 +145,15 @@ window.onload = function(){
 		setTimeout(dynamicHeights, 1000);
 	});
 }
+// END HOME PAGE JS
 
-$(document).ready(function(){
-	$(".primary-nav a, .footer-nav a, .btn").on('click', function(e) {
-		e.preventDefault();
-    	var hash = this.hash;
-    	$('body').animate({
-    		scrollTop: $(hash).offset().top
-      	}, 500, function(){
-    		window.location.hash = hash;
-      	}); 
+
+
+// PROJECTS PAGE JS
+function bodyNavigationHeight(){
+	$('.projects-selector').css('height', $('.projects-description').outerHeight());
+
+	$(window).resize(function(){
+		setTimeout(bodyNavigationHeight, 1000);
 	});
-});
+}
